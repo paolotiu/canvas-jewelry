cb3210d46ed4709782b0b73a2605aa4da2c74edc
Paolo Tiu <paolotiu17@gmail.com>
2021-04-25 17:44:46 +0800

    client side fetching instead of server side

--- components/Admin/Dashboard/Dashboard.tsx
+++ components/Admin/Dashboard/Dashboard.tsx
@@ -1,10 +1,41 @@
+import Layout from '@components/Admin/Layout/Layout';
+import Button from '@components/General/Button';
 import Table from '@components/Table/Table';
+import styled from '@emotion/styled';
 import { ItemInterface } from '@models/Item';
+import Link from 'next/link';
 import React from 'react';
 
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  padding: 1rem;
+  .header {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-self: center;
+  }
+`;
 interface Props {
-  items: ItemInterface[];
+  title: string;
+  items?: ItemInterface[];
 }
-const Dashboard = ({ items }: Props) => <Table items={items} />;
+
+const Dashboard = ({ items, title }: Props) => (
+  <Layout>
+    <Wrapper>
+      <div className="header">
+        <h1>{title}</h1>
+        <Link href="/admin/add">
+          <Button size="sm" borderRadius="md" withBorder>
+            + Add Item
+          </Button>
+        </Link>
+      </div>
+      {items && <Table items={items} />}
+    </Wrapper>
+  </Layout>
+);
 
 export default Dashboard;
--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,32 +1,13 @@
 import Dashboard from '@components/Admin/Dashboard/Dashboard';
-import Item from '@models/Item';
-import { cleanMongoData } from '@utils/cleanMongoData';
+import { ItemInterface } from '@models/Item';
 import { getItems } from '@utils/queries';
-import { connectDb } from '@utils/withMongoose';
-import { GetServerSideProps } from 'next';
 import React from 'react';
-import { QueryClient, useQuery } from 'react-query';
-import { dehydrate } from 'react-query/hydration';
+import { useQuery } from 'react-query';
 
 const DashboardPage = () => {
-  const { data } = useQuery('items', () => getItems());
+  const { data } = useQuery<ItemInterface[]>('items', () => getItems());
 
-  return <Dashboard items={data} />;
+  return <Dashboard title="Items" items={data} />;
 };
 
 export default DashboardPage;
-
-const getItemsFromDb = async () => {
-  const res = await Item.find();
-  return cleanMongoData(res);
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  await connectDb();
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery('items', () => getItemsFromDb());
-
-  return {
-    props: { dehydratedState: dehydrate(queryClient) },
-  };
-};
