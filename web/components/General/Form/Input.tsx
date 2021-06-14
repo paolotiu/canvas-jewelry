35fb89dea39b9663ff733c50842af180227a363c
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-14 17:47:27 +0800

    deleted form

--- web/components/General/Form/Input.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const Input = styled.input`
-  border-radius: ${({ theme }) => theme.borderRadius.none};
-  padding: 0.5em 1em;
-  border: 1px solid ${({ theme }) => theme.colors.gray}; ;
-`;
-export default Input;
