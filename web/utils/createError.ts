6eebace7cb3e33b9f9e1aeaacb48b1d784d4fea3
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-14 15:45:17 +0800

    deleted custom cms stuff

--- web/utils/createError.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiResponse } from 'next';
-
-export const createError = (res: NextApiResponse, code: number, message: string) => {
-  res.statusCode = code;
-  res.json({ status: code, message });
-};
--- web/utils/validationSchemas.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-export const ItemSchema = yup.object().shape({
-  name: yup.string().required().max(70),
-  price: yup.number().typeError('price must be a number').required(),
-  description: yup.string(),
-});
