fdf6fa6524973cf79422f7ac58c1a11d94b1daad
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-14 20:24:33 +0800

    updated

--- web/interfaces/index.ts
+++ web/interfaces/index.ts
@@ -3,27 +3,6 @@
 // example, to import the interface below do:
 //
 // import { User } from 'path/to/interfaces';
-
-import { CategoryInterface } from '@models/Category';
-import { ItemInterface } from '@models/Item';
-
-export type UserJWTPayload = {
-  name: string;
-  email: string;
-  admin: boolean;
-};
-
-export interface ItemQueryReturn {
-  item: ItemData;
-}
-
-export interface CategoryData extends CategoryInterface {
-  _id: string;
-}
-export interface ItemData extends ItemInterface {
-  _id: string;
-}
-
 export interface Image {
   url: string;
 }
