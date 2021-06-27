f6a0755a42b675cda64e9e49fcd610c07d2db64c
Paolo Tiu <paolotiu17@gmail.com>
2021-06-27 23:33:50 +0800

    use env var for preview url

--- sanity-studio/resolveProductionUrl.ts
+++ sanity-studio/resolveProductionUrl.ts
@@ -1,5 +1,6 @@
 const previewSecret = 'COOLBROBRO'; // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000';
+const projectUrl =
+  process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000';
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}&type=${document._type}`;
