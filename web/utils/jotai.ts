7f800d226357dec1d5d7fe238fcba583cd47c700
Paolo Tiu <paolotiu17@gmail.com>
2021-06-29 16:42:03 +0800

    used atomWithStorage jotai util

--- web/utils/jotai.ts
+++ web/utils/jotai.ts
@@ -1,9 +1,9 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
+
 import { ProductVariant } from './sanity/queries';
 
-const isSSR = typeof window === 'undefined';
 export const previewAtom = atom(false);
 export const productVariantAtom = atom<ProductVariant | null>(null);
-export const priceRevealAtom = atom(
-  isSSR ? false : JSON.parse(localStorage.getItem('isPriceRevealed') || 'false'),
-);
+
+export const priceRevealAtom = atomWithStorage('isPriceRevealed', false);
