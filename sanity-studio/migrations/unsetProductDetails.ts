87b42f56589a8a8ca9b130b93a6114ae1273b9bb
Paolo Tiu <paolotiu17@gmail.com>
2021-07-05 14:29:26 +0800

    migrations

---
+++ sanity-studio/migrations/unsetProductDetails.ts
@@ -0,0 +1,54 @@
+import sanityClient from 'part:@sanity/base/client';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('1234567890abcdef', 10);
+
+const client = sanityClient.withConfig({ apiVersion: '2019-05-28' });
+
+const fetchDocuments = () =>
+  client.fetch(
+    `*[_type == 'product'] {defaultVariant, optionsSwitch, variants, _rev, _id, }`
+  );
+
+const buildPatches = (docs) =>
+  docs.map((doc) => ({
+    id: doc._id,
+    patch: {
+      unset: ['defaultVariant', 'optionsSwitch', 'variants'],
+      // this will cause the transaction to fail if the documents has been
+      // modified since it was fetched.
+      ifRevisionID: doc._rev,
+    },
+  }));
+
+const createTransaction = (patches) =>
+  patches.reduce(
+    (tx, patch) => tx.patch(patch.id, patch.patch),
+    client.transaction()
+  );
+
+const commitTransaction = (tx) => tx.commit();
+
+const migrateNextBatch = async () => {
+  const documents = await fetchDocuments();
+  const patches = buildPatches(documents);
+
+  if (patches.length === 0) {
+    console.log('No more documents to migrate!');
+    return null;
+  }
+
+  console.log(
+    `Migrating batch:\n %s`,
+    patches
+      .map((patch) => `${patch.id} => ${JSON.stringify(patch.patch)}`)
+      .join('\n')
+  );
+  const transaction = createTransaction(patches);
+  await commitTransaction(transaction);
+};
+
+migrateNextBatch().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
