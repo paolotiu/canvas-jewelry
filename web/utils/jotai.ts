c9cbc451018ee868e9b58b01a602eb9cc1bf3c12
Paolo Tiu <paolotiu17@gmail.com>
2021-07-06 22:31:56 +0800

    added cart atoms

--- web/utils/jotai.ts
+++ web/utils/jotai.ts
@@ -1,8 +1,9 @@
+import { Cart } from '@chec/commerce.js/types/cart';
 import { SortModes } from '@components/Card/sortFunctions';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
-import { CommerceProductVariant } from './commerce/commerce';
+import { commerce, CommerceProductVariant } from './commerce/commerce';
 
 export const previewAtom = atom(false);
 export const productVariantAtom = atom<CommerceProductVariant | null>(null);
@@ -10,18 +11,62 @@ export const sortModeAtom = atom<SortModes>('stringAsc');
 
 export const priceRevealAtom = atomWithStorage('isPriceRevealed', false);
 
-type PossibleAttribute = 'size' | 'color' | 'letters' | 'additional';
-export interface CartItem {
-  id: string;
-  name: string;
-  price: number;
-  quantity: number;
-  attributes: { [key in PossibleAttribute]?: number | string };
+export const isCartOpenAtom = atom(false);
+
+export interface CartItem extends CommerceProductVariant {
   image: SanityImageSource;
-  configId: string;
+  name: string;
 }
 
-export const cartAtom = atomWithStorage<CartItem[]>('cart', []);
+interface CartBase {
+  isLoading: boolean;
+  isFetching: boolean;
+  isError: boolean;
+  data: null | Cart;
+}
+interface CartFetchingResult extends CartBase {
+  isLoading: true;
+  isError: false;
+}
+
+interface CartSuccessResult extends CartBase {
+  isLoading: false;
+  isError: false;
+  data: Cart;
+}
+
+const cartResultAtom = atom<CartFetchingResult | CartSuccessResult>({
+  isLoading: true,
+  isError: false,
+  isFetching: true,
+  data: null,
+});
+
+export const cartAtom = atom(
+  (get) => get(cartResultAtom),
+  (_, set, updatedCart: Cart | undefined | 'fetch') => {
+    if (updatedCart === 'fetch') {
+      // Trigger fetching
+      return set(cartResultAtom, (prev) => ({ ...prev, isFetching: true }));
+    }
+
+    if (updatedCart) {
+      // Fetching done
+      return set(cartResultAtom, (prev) => ({ ...prev, data: updatedCart, isFetching: false }));
+    }
+
+    set(cartResultAtom, (prev) => ({ ...prev, isFetching: true }));
+    const getInitialCart = async () => {
+      const cart = await commerce.cart.retrieve();
+      set(cartResultAtom, { data: cart, isError: false, isFetching: false, isLoading: false });
+    };
+
+    getInitialCart();
+  },
+);
+cartAtom.onMount = (update) => {
+  update();
+};
 if (process.env.NODE_ENV !== 'production') {
   cartAtom.debugLabel = 'cart';
 }
